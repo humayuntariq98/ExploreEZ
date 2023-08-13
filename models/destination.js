4c497343e206156a8e0b2fd3870a9568eef3f8bc
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-13 12:57:12 -0400

    create models

---
+++ models/destination.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema ({
+    date : {
+        type: Date,
+        required: true
+    }, 
+    hotels : {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true,
+    },
+    food : {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true,
+    },
+    summary: {
+        type: String,
+    }
+}, {
+timestamps: true,
+})
+
+const destinationSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    favoriteSpots: {
+        type: String,
+    },
+    budget: {
+        type: Number,
+        min: 1000,
+        max: 1000000,
+    },
+    reviews: [ReviewSchema],
+}, {
+    timestamps: true,
+} );
+
+ module.exports = mongoose.model('Destination', destinationSchema);
--- routes/destinations.js
+++ routes/destinations.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const destinationsCtrl = require('../controllers/destinations')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', destinationsCtrl.index)
 
 module.exports = router;
