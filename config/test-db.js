66c9b6565679d17ee7274c997fe81aec8c35fb73
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-13 13:33:21 -0400

    all destinations index page

---
+++ config/test-db.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+require('./database')
+
+
+const Destination = require('../models/destination')
+
+async function createDestination(data){
+try {
+    const newDestination = await Destination.create(data)
+    console.log(newDestination)
+} catch (error) {
+    console.log(error)
+}
+}
+
+const testData = {
+    name: 'Shanghai',
+}
--- models/destination.js
+++ models/destination.js
@@ -39,7 +39,7 @@ const destinationSchema = new Schema ({
         min: 1000,
         max: 1000000,
     },
-    reviews: [ReviewSchema],
+    reviews: [reviewSchema],
 }, {
     timestamps: true,
 } );
