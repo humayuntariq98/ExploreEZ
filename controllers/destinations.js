54f3ba5dd9c6329e700def6dac1ed4da5b09cf73
humayuntariq98 <hammytar23@gmail.com>
2023-08-14 18:44:31 +0500

    adding create functionality

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -3,6 +3,7 @@ const Destination = require('../models/destination')
 module.exports = {
     index,
     newDestination,
+    create,
 }
 
 async function index(req, res){
@@ -22,6 +23,17 @@ async function newDestination (req,res) {
     res.render ('destinations/new' , {
       title : "Add a new destination"
     })
+  } catch (error) {
+    handleError(res, 'something went wrong', error)  
+  }
+}
+
+async function create (req,res){
+  const destinationData = {...req.body}
+  try {
+    const newDestination = await Destination.create(destinationData)
+    // console.log("creatingDestination", newDestination)
+    res.redirect('/destinations')
   } catch (error) {
     handleError(res, 'something went wrong', error)  
   }
--- routes/destinations.js
+++ routes/destinations.js
@@ -7,4 +7,6 @@ router.get('/', destinationsCtrl.index)
 
 router.get('/new', destinationsCtrl.newDestination)
 
+router.post('/', destinationsCtrl.create)
+
 module.exports = router;
