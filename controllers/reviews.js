d5325ac900d08416caf89a6b072c657f065b58ee
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-14 16:18:00 -0400

    start index styling

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -26,11 +26,16 @@ async function removeReview (req,res) {
     // console.log("my name is humayun")
     try {
         const destinationToAccess = await Destination.findById(req.params.id)
+        const reviewToDelete = req.query.reviewid
+        if (!reviewIdToDelete) {
+          return res.status(400).send("Review ID not provided");
+        }
         // review to delete should have the mongo id made available from the show pag. you can pass it as req.params or you can pass it through the form as req.body
-        console.log("Review Index", destinationToAccess)
-        const reviewIndexToDelete = destinationToAccess.reviews.findIndex(req.params.reviewid)
-        console.log("Review Index", reviewIndexToDelete)
-        destinationToAccess.reviews.splice(reviewIndexToDelete,0)
+        const reviewIndex = Destination.reviews.findIndex((review) => {
+          review._id.toString() === reviewIdToDelete
+          
+        })
+      destinationToAccess.reviews.pull(req.params.reviewid)
         await destinationToAccess.save()
         res.redirect (`/destinations/${req.params.id}`)
       } catch (error) {
