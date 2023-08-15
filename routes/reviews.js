58925e527cb7537822562dc0e0c00de5a49f9597
humayuntariq98 <hammytar23@gmail.com>
2023-08-15 07:07:29 +0500

    include authentication with Google

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const reviewsCtrl = require('../controllers/reviews')
 
 router.post('/destinations/:id/reviews', reviewsCtrl.create)
-router.delete('destinations/:id/reviews/:reviewid', reviewsCtrl.removeReview)
+router.delete('/destinations/:destinationId/reviews/:reviewId', reviewsCtrl.removeReview);
 
 
 
