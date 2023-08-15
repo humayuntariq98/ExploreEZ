e7570c3664b8816657c884f4d05956e5b90d248e
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-15 12:34:28 -0400

    add google places api, css styling index page

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const Destination = require('../models/destination');
 const destinationsCtrl = require('../controllers/destinations')
 /* GET users listing. */
+
 
 router.get('/', destinationsCtrl.index)
 
--- util.js
+++ util.js
@@ -1,6 +1,5 @@
-function handleError(res, message, error){
-    res.render('/error', {
-        message,
-        error
-    })
+function handleError(res, message, error) {
+    console.error(error);
+    res.status(500).json({ error: message });
 }
+module.exports = { handleError };
