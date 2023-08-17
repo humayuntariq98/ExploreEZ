50e126bf46edea7dcbf8690643397f26fb0e17cb
humayuntariq98 <hammytar23@gmail.com>
2023-08-17 06:04:00 +0500

    formatting all files

--- util.js
+++ util.js
@@ -1,12 +1,12 @@
 
 function handleError(res, message, error){
-    res.render('/error', {
+    res.render("/error", {
         message,
         error
-    })
+    });
 }
 
 module.exports = {
     handleError
-}
+};
 
